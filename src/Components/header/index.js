f1597c20e15f8ad8824865c175b0dbc794fe4f82
MohammadAltamimi98 <m98altamimi@gmail.com>
2021-07-17 19:02:15 +0300

    commit

--- src/Components/header/index.js
+++ src/Components/header/index.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-function Header() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>Handy-Man online service</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/client">Client</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+// import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap/';
+import { Link, useHistory } from 'react-router-dom';
 
+
+
+function Header() {
+
+  const history = useHistory();
+
+  function logOut() {
+    localStorage.clear();
+    history.push('/');
+
+  }
+
+  return (
+    <div>
+      <Navbar bg="primary" variant="dark">
+
+        <Navbar.Brand href="#home">HandyMan</Navbar.Brand>
+
+        <Nav className="me-auto nav_bar_wrapper">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/client">Client</Nav.Link>
+          <Nav.Link href="/admin">Admin</Nav.Link>
+        </Nav>
+        {localStorage.getItem('token') ?
+          <Nav>
+            <NavDropdown title="user name">
+              <NavDropdown.Item onClick={logOut}>Log Out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav> : null
+        }
+
+
+      </Navbar>
+    </div>
+  )
+
+}
 export default Header;
